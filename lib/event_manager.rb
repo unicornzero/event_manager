feebf69016c51d4ce0104da71c53d0c74ceec6a6
unicornzero <rylee.k@gmail.com>
2013-06-07 13:33:42 -0600

    implemented ruby CSV lib

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,10 +1,10 @@
+require "csv"
 puts "Event Manager initialized"
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line,index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
   puts name
-  index
 end
+
+
