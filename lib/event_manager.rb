8b80e0b2cd15ef0eeb7b7051c0145b2c6adce275
unicornzero <rylee.k@gmail.com>
2013-06-07 16:52:25 -0600

    added erb-based thank you letter generation

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
@@ -8,27 +9,35 @@ def clean_zipcode(zipcode)
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
-template_letter = File.read "form_letter.html"
+def save_thank_you_letter(id,form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
+end
 
 puts "Event Manager initialized"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
-
   zipcode = clean_zipcode(row[:zipcode])
-    
-  legislators = legislators_by_zipcode(zipcode).join(",")
+  legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id,form_letter)
 end
 
 
